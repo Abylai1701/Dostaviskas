e5419c66e701e6d5460aac6995cc5d8a01b55565
Abylai <abilkairablai@gmail.com>
2025-11-14 17:23:55 +0500

    second commit

--- Dostavista/App/DostavistaApp.swift
+++ Dostavista/App/DostavistaApp.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
+import AppTrackingTransparency
 
 @main
 struct DostavistaApp: App {
+    
+    @Environment(\.scenePhase) var scenePhase
+
     var body: some Scene {
         
         WindowGroup {
             RootView()
                 .preferredColorScheme(.light)
+                .onChange(of: scenePhase) {
+                    if scenePhase == .active {
+                        ATTrackingManager.requestTrackingAuthorization { status in
+                            switch status {
+                            case .authorized:
+                                print("IDFA granted")
+                            case .denied, .restricted, .notDetermined:
+                                print("IDFA authorization not granted")
+                            @unknown default:
+                                break
+                            }
+                        }
+                    }
+                }
         }
     }
 }
